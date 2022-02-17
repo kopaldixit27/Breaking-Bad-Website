b2e22ddc46a2ba20f12e4d1728d07679adc91a7e
kopaldixit27 <76914395+kopaldixit27@users.noreply.github.com>
2022-02-17 23:18:06 +0530

    All files added

---
+++ src/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import spinner from '../img/Spinner.gif'
+
+const Spinner = () => {
+  return (
+    <img src={spinner} alt='Loading...' style={{width:'200px' , margin:'auto', display:'block' }}/>
+  )
+}
+
+export default Spinner
